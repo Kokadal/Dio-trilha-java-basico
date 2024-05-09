65272d3fa770bf10c666d5ad83052debd76648f0
DouglasRoberto <dodo.douglas09@hotmail.com>
2024-05-08 23:57:00 -0300

    Alterações no codigo

--- BoletimEscolar/BoletimEscolar.java
+++ BoletimEscolar/BoletimEscolar.java
@@ -7,17 +7,16 @@ public class BoletimEscolar {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("7");
+        System.out.println("Digite a nota do aluno:");
         double nota = scanner.nextDouble();
         
         if (nota >= 7.0) {
             System.out.println("Aluno aprovado!");
         } else {
-            if (nota <= 7.0)
             System.out.println("Aluno reprovado.");
         }
         
         scanner.close();
-}
+    }
 
 }
