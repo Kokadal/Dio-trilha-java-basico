1aea9295ba9746b0b77d1c730265adc229d41da6
DouglasRoberto <dodo.douglas09@hotmail.com>
2024-05-08 23:29:44 -0300

    Ajustes finais conforme  solicitado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,7 +4,7 @@ public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Bem vindo ao banco Roberth. O que podeos fazer por você hoje? Por favor, digite começar para entrar em sua conta");
+        System.out.println("Bem vindo ao banco Roberth. O que podeos fazer por você hoje? Por favor, digite COMEÇAR para entrar em sua conta");
         String começar = scanner.next();
 
         System.out.println("Por favor, me diga seu nome:");
@@ -22,13 +22,30 @@ public class ContaTerminal {
             System.out.println("Acesso autorizado " + nome + ", Bem vinde. No momento, sua conta tem apenas a opção Saque disponivel. Para consultar o valor digite SALDO");
             String acesso = scanner.next();
 
-            System.out.println("R$50,00 ");
-            double saldo = scanner.nextDouble();
+            double limiteSaque = 50.00;
 
-            System.out.println("Seu limite disponivel saque é de" + saldo );
+        // Verificando a opção escolhida pelo usuário
+        if (acesso.equalsIgnoreCase("SALDO")) {
+            System.out.println("Seu saldo disponível é de R$ " + limiteSaque);
+        } else {
+            System.out.println("Opção inválida!");
+        }
+
+        // Se o usuário escolher sacar, continua com a lógica do saque
+        if (acesso.equalsIgnoreCase("SALDO")) {
+            System.out.println("Por favor, informe o valor que deseja sacar");
+            double valorSolicitado = scanner.nextDouble();
+
+            if (limiteSaque >= valorSolicitado) {
+                limiteSaque -= valorSolicitado;
+                System.out.println("Transação realizada com sucesso! Novo saldo: R$ " + limiteSaque);
+            } else {
+                System.out.println("Saldo insuficiente");
+            }
 
             scanner.close();
 
 
+       }
     }
 }
