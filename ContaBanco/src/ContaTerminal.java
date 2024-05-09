587371d7d4b98a4027ac5f147ab752c08cdb913f
DouglasRoberto <dodo.douglas09@hotmail.com>
2024-05-08 21:49:42 -0300

    Alteração ReadME

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,12 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Conhecer e importar a classe Scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela classe Scanner os valores digitados no termial
+
+        //Exibir a mensagem da conta criada
+
     }
 }
