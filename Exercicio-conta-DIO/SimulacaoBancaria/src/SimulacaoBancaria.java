db0a8bcfd3e411e5462a4b6282c579e176e78bef
DouglasRoberto <dodo.douglas09@hotmail.com>
2024-05-17 22:02:49 -0300

    desafios completos

---
+++ Exercicio-conta-DIO/SimulacaoBancaria/src/SimulacaoBancaria.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+
+        while (true) {
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    double valorDeposito = scanner.nextDouble();
+                    saldo += valorDeposito;
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+
+                case 2:
+                    double valorSaque = scanner.nextDouble();
+                    if (saldo >= valorSaque) {
+                        saldo -= valorSaque;
+                        System.out.println("Saldo atual: " + saldo);
+                    } else {
+                        System.out.println("Saldo insuficiente.");
+                        System.out.println("Programa encerrado.");
+                        scanner.close();
+                        System.exit(0);
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+    }
+}
